b0d8902452963a0851d48d4349099f2b0057563e
manideep <17101992m@gmail.com>
2022-11-22 23:03:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+import * as dotenv from 'dotenv';
+import express from "express";
+import dbConnection from "./config/db.js";
+import routes from './routes/routes.js'
+
+dotenv.config()
+const app = express();
+dbConnection();
+
+// Middleware
+app.use(express.json())
+app.use("/", routes)
+
+export default app
