89bbe4a6a539ee7019fea2aff2d1e4a3a2c19bea
manideep <17101992m@gmail.com>
2022-11-23 12:40:40 +0530

    route added for the addTitle

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import { addTitle } from '../controllers/addTitle.js'
 import { createUser } from '../controllers/createUser.js'
 import { home } from '../controllers/home.js'
 const router = express.Router()
 
 router.get('/', home)
 router.post('/createUser', createUser)
+router.put('/addTitle/:id', addTitle)
 
 export default router
