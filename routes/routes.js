7e39f70ec931f8f31929998432790ae423992072
manideep <17101992m@gmail.com>
2022-11-24 00:31:46 +0530

    route added for task updation

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { addTask } from '../controllers/addTask.js'
 import { addTitle } from '../controllers/addTitle.js'
 import { createUser } from '../controllers/createUser.js'
 import { home } from '../controllers/home.js'
@@ -7,5 +8,6 @@ const router = express.Router()
 router.get('/', home)
 router.post('/createUser', createUser)
 router.put('/addTitle/:id', addTitle)
+router.put('/addTask/:id/:titleId', addTask)
 
 export default router
