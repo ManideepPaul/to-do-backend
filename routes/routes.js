7b901e3a107f862ef1d4b375a23c0931e38459f6
manideep <17101992m@gmail.com>
2022-11-24 09:40:07 +0530

    route added for the delete task

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { addTask } from '../controllers/addTask.js'
 import { addTitle } from '../controllers/addTitle.js'
 import { createUser } from '../controllers/createUser.js'
+import { deleteTask } from '../controllers/deleteTask.js'
 import { home } from '../controllers/home.js'
 const router = express.Router()
 
@@ -9,5 +10,6 @@ router.get('/', home)
 router.post('/createUser', createUser)
 router.put('/addTitle/:id', addTitle)
 router.put('/addTask/:id/:titleId', addTask)
+router.put('/deleteTask/:id/:titleId/:index', deleteTask)
 
 export default router
