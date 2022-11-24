6690ddd9d7de3aae28244d48c4b3a62c93541871
manideep <17101992m@gmail.com>
2022-11-24 12:12:52 +0530

    deleteTitle route added

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,7 @@ import { addTask } from '../controllers/addTask.js'
 import { addTitle } from '../controllers/addTitle.js'
 import { createUser } from '../controllers/createUser.js'
 import { deleteTask } from '../controllers/deleteTask.js'
+import { deleteTitle } from '../controllers/deleteTitle.js'
 import { home } from '../controllers/home.js'
 const router = express.Router()
 
@@ -10,6 +11,7 @@ router.get('/', home)
 router.post('/createUser', createUser)
 router.put('/addTitle/:id', addTitle)
 router.put('/addTask/:id/:titleId', addTask)
-router.put('/deleteTask/:id/:titleId/:index', deleteTask)
+router.delete('/deleteTask/:id/:titleId/:index', deleteTask)
+router.delete('/deleteTitle/:id/:index', deleteTitle)
 
 export default router
