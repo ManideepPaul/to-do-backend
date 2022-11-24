47f665d4c0b8abdcedeb540132df20fdf3bdb5e0
manideep <17101992m@gmail.com>
2022-11-24 09:39:29 +0530

    deleteTask logic added

---
+++ controllers/deleteTask.js
@@ -0,0 +1,12 @@
+import User from "../models/userModel.js";
+
+export const deleteTask = async (req, res) => {
+    const resp = await User.findById(req.params.id);
+    const title = resp.title.find(obj => obj._id == req.params.titleId)
+    title.tasks.splice(req.params.index, 1);
+    await resp.save();
+    res.status(201).json({
+        success: true,
+        message: "Task deleted successfully"
+    })
+}
