549861a57a0bba8891196c3a6a8f01bacf2cf50b
manideep <17101992m@gmail.com>
2022-11-30 12:45:37 +0530

    email and hashed password will be saved on DB when creating user

--- controllers/createUser.js
+++ controllers/createUser.js
@@ -1,15 +1,31 @@
-import User from "../models/userModel.js"
+import User from "../models/userModel.js";
+import bcrypt from "bcrypt"
 
 export const createUser = async (req, res) => {
     try {
-        const { name } = req.body;
-        console.log(name);
+        const { name, password, email } = req.body;
+        const lowerEmail = email.toLowerCase()
         
         // Checking if name exist
-        if(!name) res.send("Name required")
+        if(!(name && password && email)) res.send("All fields are required")
+
+        // Check is email exist
+        const emailExist = await User.findOne({email: lowerEmail});
+
+        if(emailExist) res.json({
+            success: false,
+            message: "User already exist"
+        })
+
+        // Hashing the password
+        const hashPassword = await bcrypt.hash(password, 10)
 
         // Inserting data into database
-        const user = await User.create({name})
+        const user = await User.create({
+            name,
+            email: lowerEmail,
+            password: hashPassword
+        })
         res.status(201).json({
             success: true,
             message: "User name added successfully",
