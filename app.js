cb711297bc4f611a2407f3770f26ce30a15124a4
manideep <17101992m@gmail.com>
2022-12-01 09:46:56 +0530

    cookie-parser added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import * as dotenv from 'dotenv';
 import express from "express";
 import dbConnection from "./config/db.js";
-import routes from './routes/routes.js'
+import routes from './routes/routes.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 const app = express();
@@ -10,6 +11,7 @@ dbConnection();
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use("/", routes)
 
 export default app
