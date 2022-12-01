512aebf0e6cba5a75db11777ee644a8e6ea29e05
manideep <17101992m@gmail.com>
2022-12-01 09:45:33 +0530

    middleware added before getting user data

---
+++ middleware/auth.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+/* 
+    Steps
+    - Get the token from cookies
+    - If token is not present then throw error
+    - verify the token
+    - then use next()
+*/
+
+export const auth = async (req, res, next) => {
+    const token =  req.body.token || req.cookies.token;
+    try {
+        if(!token) res.status(401).json({
+            success: false,
+            message: 'Token missing'
+        })
+        const verifyToken = jwt.verify(token, process.env.SECRET_KEY)
+        console.log(verifyToken)
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid token"
+        })
+    }
+
+    return next()
+}
