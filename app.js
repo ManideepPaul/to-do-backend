3417df3b8e88ab7a8c312325a4d0f65a12d9ded2
Manideep <17101992m@gmail.com>
2022-12-05 13:33:28 +0530

    credentials and origin added in cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 dbConnection();
 
 // Middleware
-app.use(cors())
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
