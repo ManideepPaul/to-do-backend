510f273f82be687a964100dc3c1076b8c29b17dc
Manideep <17101992m@gmail.com>
2022-12-05 13:35:56 +0530

    bug removed - if token is not found it is still running the code in middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,10 +11,12 @@ import jwt from 'jsonwebtoken';
 export const auth = async (req, res, next) => {
     const token =  req.body.token || req.cookies.token;
     try {
-        if(!token) res.status(401).json({
-            success: false,
-            message: 'Token missing'
-        })
+        if(!token) {   
+            return res.status(401).json({
+                success: false,
+                message: 'Token missing'
+            })
+        } 
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY)
         req.dataFromMiddleware = verifyToken.userId
     } catch (error) {
